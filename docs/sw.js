2d42a2de8617c6955f7d851a4ee053c0047fec51
Rick Jones <rickjjones@gmail.com>
2025-09-26 15:34:55 -0400

    version increment

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 // service worker for docs/ site (use relative paths so it works on GitHub Pages project sites)
-const CACHE_NAME = 'emotion-tracker-v4';
+const CACHE_NAME = 'emotion-tracker-v5';
 const ASSETS = [
   './',
   'index.html',
